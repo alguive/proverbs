e817cbe794b673f025aa48a11e47ad83903fa795
alguive <a.guijarro.vera@gmail.com>
2025-05-30 22:11:39 +0200

    Add migration message

--- migrations/Version20250530195944.php
+++ migrations/Version20250530195944.php
@@ -14,7 +14,7 @@ final class Version20250530195944 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create proverb & proverb_content tables';
     }
 
     public function up(Schema $schema): void
